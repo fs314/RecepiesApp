96e7f4fc5cefc8ec5c460d7eeae87b95223e4f50
Flaminia Salustri <flaminias@UKHOITMB3862.local>
2023-01-04 16:04:24 +0100

    add useAuth and useRefreshToken

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,14 @@
-import "./App.css";
 import React from "react";
-import Pages from "./pages/Pages";
+import { AuthProvider } from "./context/useAuth";
+import LoginAndRegistrationForm from "./pages/LoginAndRegistrationForm";
 
 function App() {
   return (
     <div className="App">
       <div className="text-3xl font-bold underline">HEADER</div>
-      <div>SEARCH BAR</div>
-      <Pages />
+      <AuthProvider>
+        <LoginAndRegistrationForm />
+      </AuthProvider>
     </div>
   );
 }
