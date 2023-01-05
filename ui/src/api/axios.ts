0f8696fc4b1a334c33737bb6ee88c484f7604087
Flaminia Salustri <flaminias@UKHOITMB3862.Home>
2023-01-05 23:54:47 +0100

    add hook for private routes token refresh

--- ui/src/api/axios.ts
+++ ui/src/api/axios.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
+import { BASE_URL } from "../config/urlConfig";
 
 //setting base url in one single place
 export default axios.create({
-  baseURL: "http://localhost:4050",
+  baseURL: BASE_URL,
+});
+
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
 });
